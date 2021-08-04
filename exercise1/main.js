01876f675fef78dab494aacbe7790262ae6a071a
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-04 15:51:08 +0200

    Ex1 - change display from list-item to block and set starting font-size as 10px

--- exercise1/main.js
+++ exercise1/main.js
@@ -2,12 +2,14 @@ const btn = document.querySelector("button");
 const allLi = document.querySelectorAll("li");
 const firstLi = document.querySelector("li");
 
-let size = 16;
+let size = 10;
 
 btn.addEventListener("click", () => {
   if (firstLi.style.display == "") {
-    allLi.forEach((li) => (li.style.display = "list-item"));
-    console.log(firstLi.style.display);
+    allLi.forEach((li) => {
+      li.style.display = "block";
+      li.style.fontSize = size + "px";
+    });
   } else {
     size++;
     allLi.forEach((li) => (li.style.fontSize = size + "px"));
