f33e322e66d54ed6f7e21e32a990ea09f52c66f8
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-04 16:00:56 +0200

    Ex1 - add an exercise solution using a for loop

--- exercise1/main.js
+++ exercise1/main.js
@@ -4,14 +4,32 @@ const firstLi = document.querySelector("li");
 
 let size = 10;
 
+//********** forEach **********
+
+// btn.addEventListener("click", () => {
+//   if (firstLi.style.display == "") {
+//     allLi.forEach((li) => {
+//       li.style.display = "block";
+//       li.style.fontSize = size + "px";
+//     });
+//   } else {
+//     size++;
+//     allLi.forEach((li) => (li.style.fontSize = size + "px"));
+//   }
+// });
+
+// ********** for **********
+
 btn.addEventListener("click", () => {
   if (firstLi.style.display == "") {
-    allLi.forEach((li) => {
-      li.style.display = "block";
-      li.style.fontSize = size + "px";
-    });
+    for (i = 0; i < 10; i++) {
+      allLi[i].style.display = "block";
+      allLi[i].style.fontSize = size + "px";
+    }
   } else {
     size++;
-    allLi.forEach((li) => (li.style.fontSize = size + "px"));
+    for (i = 0; i < 10; i++) {
+      allLi[i].style.fontSize = size + "px";
+    }
   }
 });
