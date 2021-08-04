517b798463ad662b9ac29953ad49c53bf2b0986a
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-04 16:55:11 +0200

    Ex2 - init function done

--- exercise2/main.js
+++ exercise2/main.js
@@ -2,7 +2,14 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
-  //code
+  const btn = document.createElement("button");
+  btn.textContent = "Dodaj 10 elementów listy";
+  document.body.appendChild(btn);
+
+  const ul = document.createElement("ul");
+  document.body.appendChild(ul);
+
+  btn.addEventListener("click", createLiElements);
 };
 
 const createLiElements = () => {
