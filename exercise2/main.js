6f9f64e1a761f798d1507e04ab6c8fa470fdb1fb
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-04 18:16:47 +0200

    Ex2 - fixed typo

--- exercise2/main.js
+++ exercise2/main.js
@@ -3,7 +3,7 @@ let orderElement = 1;
 
 const init = () => {
   const btn = document.createElement("button");
-  btn.textContent = "Dodaj 10 elementów listy";
+  btn.textContent = "Dodaj 10 elementów do listy";
   document.body.appendChild(btn);
 
   const ul = document.createElement("ul");
